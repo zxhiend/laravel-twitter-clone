ee84abd6d46af1bdb086632a3d8eb097aa4efc64
BeSalt1080 <wijayashinichi@gmail.com>
2025-06-04 23:28:44 +0700

    feat: add like, reply, and retweet resource

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\ReplyController;
+use App\Http\Controllers\RetweetController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::resource('like', LikeController::class)->only(['create', 'destroy']);
+    Route::resource('reply', ReplyController::class)->only(['create', 'destroy']);
+    Route::resource('retweet', RetweetController::class)->only(['create', 'destroy']);
 });
