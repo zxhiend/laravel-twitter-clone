8f40761a0f17896c871bfe1b8bd2f879a61dbac8
C240 <cantonae240@gmail.com>
2025-06-19 10:21:29 +0700

    fix tampilan pada menu followings dan followers

--- resources/views/users/followings.blade.php
+++ resources/views/users/followings.blade.php
@@ -20,14 +20,14 @@
                     @foreach($followings as $following)
                     <div class="d-flex justify-content-between align-items-center mb-3">
                         <div class="d-flex align-items-center">
-                            <img src="{{ $following->avatar ?? asset('images/default-avatar.png') }}" 
+                            <img src="{{ $following->avatar ? asset('storage/' . $following->avatar) : asset('images/default-avatar.png') }}" 
                                  class="rounded-circle mr-3" 
                                  width="50" 
                                  height="50" 
                                  alt="{{ $following->name }}">
                             <div>
                                 <h5 class="mb-0">{{ $following->name }}</h5>
-                                <small class="text-muted">@{{ $following->username }}</small>
+                                <small class="text-muted">&commat;{{ $following->username }}</small>
                             </div>
                         </div>
 
